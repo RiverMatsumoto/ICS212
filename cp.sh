68ff3e99782b4fd1c09b425a891e26fa4bd1573f
RiverMatsumoto <matsumotorjames@gmail.com>
2022-10-06 13:42:26 -1000

    finished homework5

---
+++ cp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp template.c $1
+mv $1/template.c $2
---
+++ template.c
@@ -0,0 +1,36 @@
+/*****************************************************************
+//
+//  NAME:        River Matsumoto
+//
+//  HOMEWORK:    1
+//
+//  CLASS:       ICS 212
+//
+//  INSTRUCTOR:  Ravi Narayan
+//
+//  DATE:        
+//
+//  FILE:        
+//
+//  DESCRIPTION:
+//
+****************************************************************/
+
+
+/*****************************************************************
+//
+//  Function name: main
+//
+//  DESCRIPTION:   
+//
+//  Parameters:    argc (int) : The number of elements in argv
+//                 argv (char*[]) : An array of arguments passed
+//                                  to the program.
+//
+//  Return values:  0 : successfully exited the program
+//
+****************************************************************/
+
+int main(int argc, char* argv[])
+{
+}
